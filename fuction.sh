824de19ae0b12da886a79aab3b46e7deee45e7e3
jeyasundarik <jeysundhariak@gmail.com>
2022-01-01 12:23:48 +0000

    fuction is commited now

---
+++ fuction.sh
@@ -0,0 +1,12 @@
+#!/bin/bash 
+echo "system resources commands"
+cd /home/ec2-user/shellscripts/
+source ./jeya.sh
+src()
+{
+echo "uptime - this command is used for how long the server is running"
+echo "du -sh - this command is for directory size"
+echo "df -kh - this for mount points"
+echo "sudo ss -tulpn | grep tcp - for port is enable or not "
+}
+src

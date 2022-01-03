d5b3d1c0e3185b029e8b10b1cae7bb66f27a0400
jeyasundarik <96933365+jeyasundarik@users.noreply.github.com>
2022-01-03 16:25:19 +0530

    Update installjava.sh

--- installjava.sh
+++ installjava.sh
@@ -4,7 +4,5 @@ sudo su -
 cd /opt
 java -version
 yum install wget -y
-#check the latest version of java in https://www.oracle.com/java/technologies/downloads
-wget -c --header "Cookie: oraclelicense=accept-securebackup-cookie" https://download.oracle.com/otn/java/jdk/8u311-b11/4d5417147a92418ea8b615e228bb6935/jdk-8u311-linux-x64.rpm
-yum install jdk-8u311-linux-x64.rpm -y
-java -version
+wget -c --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-linux-x64.rpm
+yum install jdk-8u131-linux-x64.rpm -y
